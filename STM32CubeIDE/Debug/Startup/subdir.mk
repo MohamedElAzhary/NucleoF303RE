6c8345ef0925a7def7b9b3fa5985edbae9d11ff1
MoAzario <1malazhary@gmail.com>
2022-01-21 14:03:58 +0200

    Adding Initial Project Files
    This commit builds successfully.

---
+++ STM32CubeIDE/Debug/Startup/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Startup/stm32f303x6_startup.c 
+
+OBJS += \
+./Startup/stm32f303x6_startup.o 
+
+C_DEPS += \
+./Startup/stm32f303x6_startup.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Startup/%.o: ../Startup/%.c Startup/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F3 -DSTM32F303RETx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
